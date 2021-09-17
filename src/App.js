607fb0c9b1bea7dabb88761a9435d9d7df14e66e
M603-droid <mridulanamdeo@gmail.com>
2021-09-17 12:15:01 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -8,51 +8,23 @@ import { Row } from "react-bootstrap";
 import Artist from "./components/Artist";
 import Album from "./components/Album";
 
-let headers = new Headers({
-  "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  "X-RapidAPI-Key": "222902beabmshb95a65b737cead6p1f3ac9jsn23ced94c0d20",
-});
+
 
 class App extends React.Component {
   state = {
     searchResults: [],
   };
 
-  search = async (string) => {
-    if (string.length > 2) {
-      try {
-        let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/deezer/search?q=" +
-            string,
-          {
-            method: "GET",
-            headers,
-          }
-        );
-
-        let result = await response.json();
-        let songs = result.data;
-
-        this.setState({
-          searchResults: songs,
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
+  
 
   render() {
     return (
       <Router>
         <div className="container-fluid">
           <Row>
-            <Sidebar search={this.search} />
-            <Route
-              path="/"
-              exact
-              render={() => <Home searchResults={this.state.searchResults} />}
-            />
+            <Sidebar />
+            
+            
             <Route path="/artist/:id" component={Artist} />
             <Route path="/album/:id" component={Album} />
           </Row>
